e34464c0be09158b56e57fe6e8af2a2688ca5ec1
Markijani <markijani55@gmail.com>
2023-08-10 15:03:35 -0700

    Update "Calculator.java",update delete spaces

--- src/main/java/us/kvitko/calcapp/service/ResultWriteService.java
+++ src/main/java/us/kvitko/calcapp/service/ResultWriteService.java
@@ -5,6 +5,7 @@ public class ResultWriteService {
     public static void printResult(int num1, int num2, String addition, int result) {
         System.out.println(num1 + " " + num2 + " " + "addition" + " " + result);
     }
+    
     public static void printResult1(int num1, int num2, String subtraction, int result1) {
         System.out.println(num1 + " " + num2 + " " + "subtraction" + " " + result1);
     }
