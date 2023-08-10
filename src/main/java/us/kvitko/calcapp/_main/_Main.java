e6b9d4e7e808c11b0e971ccee451eb4c7a338440
Markijani <markijani55@gmail.com>
2023-08-10 12:25:06 -0700

    Update "Calculator.java"

---
+++ src/main/java/us/kvitko/calcapp/_main/_Main.java
@@ -0,0 +1,22 @@
+package us.kvitko.calcapp._main;
+
+import us.kvitko.calcapp.service.ResultWriteService;
+
+import static us.kvitko.calcapp.util.Calculator.*;
+
+public class _Main {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int result = add(num1, num2);
+        int result1 = subtract(num1, num2);
+        int result2 = multiply(num1, num2);
+
+        ResultWriteService.printResult(num1, num2, "addition", result);
+        ResultWriteService.printResult1(num1, num2, "subtraction", result1);
+        ResultWriteService.printResult2(num1, num2, "multiplication", result2);
+    }
+
+
+}
---
+++ src/main/java/us/kvitko/calcapp/service/ResultWriteService.java
@@ -0,0 +1,41 @@
+package us.kvitko.calcapp.service;
+
+public class ResultWriteService {
+
+    public static void printResult(int num1, int num2, String addition, int result) {
+        System.out.println(num1 + " " + num2 + " " + "addition" + " " + result);
+    }
+
+    public static void printResult1(int num1, int num2, String subtraction, int result1) {
+        System.out.println(num1 + " " + num2 + " " + "subtraction" + " " + result1);
+    }
+
+    public static void printResult2(int num1, int num2, String multiplication, int result2) {
+        System.out.println(num1 + " " + num2 + " " + "multiplication" + " " + result2);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
